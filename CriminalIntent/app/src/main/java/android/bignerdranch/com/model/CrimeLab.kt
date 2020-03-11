b24b8e613d829c633bfa95c2371d21e7039e33a9
garcian <khizhindenis@gmail.com>
2020-03-11 07:49:06 +0300

    notifyDataSetChanged is added

--- CriminalIntent/app/src/main/java/android/bignerdranch/com/model/CrimeLab.kt
+++ CriminalIntent/app/src/main/java/android/bignerdranch/com/model/CrimeLab.kt
@@ -4,26 +4,28 @@ import android.bignerdranch.com.utils.SingletonHolder
 import android.content.Context
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.collections.LinkedHashMap
 
 class CrimeLab private constructor(context: Context) {
 
-    private var _crimes = ArrayList<Crime>()
+    private var _crimes = LinkedHashMap<UUID, Crime>()
     val crimes: List<Crime>
-        get() = _crimes
+        get() = ArrayList<Crime>(_crimes.values)
 
     init {
         for (i in 0..99) {
             val crime = Crime()
             crime.title = "Crime #$i"
             crime.solved = i % 2 == 0 // Every other one
-            _crimes.add(crime)
+            _crimes[crime.id] = crime
         }
     }
 
     operator fun get(id: UUID): Crime? {
-        return crimes.firstOrNull { crime ->
-            crime.id == id
-        }
+        return if (_crimes.containsKey(id))
+            _crimes[id]
+        else
+            null
     }
 
     companion object : SingletonHolder<CrimeLab, Context>(::CrimeLab)
